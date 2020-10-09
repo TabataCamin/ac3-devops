de7a0b76898221a473d9e70ecff4605f346273eb
TabataCamin <tabatacamin@gmail.com>
2020-10-09 19:57:37 -0300

    ac

--- primos.py
+++ primos.py
@@ -25,6 +25,6 @@ def nao_entre_em_panico():
     return primos
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
